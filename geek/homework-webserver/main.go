cf0d1a1877e6c82755a6c670c8c4d6050c145cc7
CurryFu <curry.fu1@ingka.ikea.com>
2022-07-24 16:36:16 +0800

    just a quick commit

---
+++ geek/homework-webserver/main.go
@@ -0,0 +1,16 @@
+//creating a new web server
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf("%s is starting to run his code.", "Curry Fu")
+	var sli = make([]string, 10, 10)
+	for i := 0; i < 10; i++ {
+		sli[i] = strconv.Itoa(i)
+	}
+	fmt.Println(sli)
+}
