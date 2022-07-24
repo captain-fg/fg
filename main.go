57d6715b5568997696fd725cb58eaa856e5dd437
captain-fg <59461520+captain-fg@users.noreply.github.com>
2022-07-24 15:28:57 +0800

    first commit always existing

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf("%s is starting to run his code.", "Curry Fu")
+	var sli = make([]string, 10, 10)
+	for i := 0; i < 10; i++ {
+		sli[i] = strconv.Itoa(i)
+	}
+	fmt.Println(sli)
+}
